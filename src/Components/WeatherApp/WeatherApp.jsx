482fb0f5427943c1dfa0306d36e44bba9f795545
Adel SIDI AHMED <adelsidiahmed2020@gmail.com>
2023-11-08 14:49:36 +0100

    version 0.1

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react'
-import './WeatherApp.css'
+import '../WeatherApp.css'
 
-import search_icon from '../Assets/search.png'
-import clear_icon from '../Assets/clear.png'
-import cloud_icon from '../Assets/cloud.png'
-import drizzle_icon from '../Assets/drizzle.png'
-import humidity_icon from '../Assets/humidity.png'
-import rain_icon from '../Assets/rain.png'
-import snow_icon from '../Assets/snow.png'
-import wind_icon from '../Assets/wind.png'
+import search_icon from './Assets/search.png'
+import clear_icon from './Assets/clear.png'
+import cloud_icon from './Assets/cloud.png'
+import drizzle_icon from './Assets/drizzle.png'
+import humidity_icon from './Assets/humidity.png'
+import rain_icon from './Assets/rain.png'
+import snow_icon from './Assets/snow.png'
+import wind_icon from './Assets/wind.png'
 
 export const WeatherApp = () => {
 
